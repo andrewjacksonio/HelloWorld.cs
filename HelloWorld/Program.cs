371f9086dda03fb947ca8e0a265d644cfe27675b
Andrew Jackson <andrew.jackson@groupimd.com>
2016-05-22 12:54:46 +0800

    commit solution

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+            Console.ReadLine();
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
